77d3aee0922e9cb43b3338c73a400293db6f28c0
Bryan <bzuelly@outlook.com>
2022-07-08 02:57:19 +0100

    functional

--- app.py
+++ app.py
@@ -7,66 +7,83 @@ from flask import Flask, render_template
 import datetime
 import RPi.GPIO as GPIO
 
+IN1 = 25
+IN2 = 24
+ENA = 23
+IN3 = 17
+IN4 = 27
+ENB = 22
+
 GPIO.setmode(GPIO.BCM)
 
-# Create a dictionary called pins to store the pin number, name, and pin state:
-pins = {
-   25 : {'name' : 'GPIO 25: IN1', 'state' : GPIO.LOW},
-   24 : {'name' : 'GPIO 24: IN2', 'state' : GPIO.LOW},
-   23 : {'name' : 'GPIO 23: ENA', 'state' : GPIO.HIGH},
-   17 : {'name' : 'GPIO 17: IN3', 'state' : GPIO.LOW},
-   27 : {'name' : 'GPIO 27: IN4', 'state' : GPIO.LOW},
-   22 : {'name' : 'GPIO 22: ENB', 'state' : GPIO.LOW}
-   }
+def startup():
+    # Create a dictionary called pins to store the pin number, name, and pin state:
+    pins = {
+        25 : {'name' : 'GPIO 25: IN1', 'state' : GPIO.LOW},
+        24 : {'name' : 'GPIO 24: IN2', 'state' : GPIO.LOW},
+        23 : {'name' : 'GPIO 23: ENA', 'state' : GPIO.HIGH},
+        17 : {'name' : 'GPIO 17: IN3', 'state' : GPIO.LOW},
+        27 : {'name' : 'GPIO 27: IN4', 'state' : GPIO.LOW},
+        22 : {'name' : 'GPIO 22: ENB', 'state' : GPIO.LOW}
+        }
 
-# Set each pin as an output and make it low:
-for pin in pins:
-   GPIO.setup(pin, GPIO.OUT)
-   GPIO.output(pin, GPIO.LOW)
-
-
+       # Set each pin as an output and make it low:
+    for pin in pins:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.LOW)
+    return pins
 
 app = Flask(__name__)
 @app.route("/")
-def hello():
-   now = datetime.datetime.now()
-   timeString = now.strftime("%Y-%m-%d %H:%M")
-   left = 'left'
-   templateData = {
-      'time': timeString,
-      }
-   return render_template('index.html', **templateData)
+#def hello():
+#   now = datetime.datetime.now()
+#   timeString = now.strftime("%Y-%m-%d %H:%M")
+#   templateData = {
+#      'time': timeString,
+#      }
+#   return render_template('index.html', **templateData)
 
 @app.route("/forward")
 def forward():
-    GPIO.output(pins[25],GPIO.HIGH)
-    GPIO.output(pins[24],GPIO.LOW)
-    GPIO.output(pins[23],GPIO.HIGH)
-    GPIO.cleanup()     
+    startup()
+    GPIO.output(IN1,GPIO.LOW)
+    GPIO.output(IN2,GPIO.HIGH)
+    GPIO.output(ENA,GPIO.HIGH)
+    #GPIO.cleanup()
     return render_template('index.html')
 
 @app.route("/reverse")
 def reverse():
-       
+    startup()
+    GPIO.output(IN1,GPIO.HIGH)
+    GPIO.output(IN2,GPIO.LOW)
+    GPIO.output(ENA,GPIO.HIGH)
+    #GPIO.cleanup()
     return render_template('index.html')
 
 @app.route("/stop")
 def stop():
-    GPIO.output(pins[23],GPIO.LOW)
-    GPIO.output(pins[24],GPIO.LOW)
-    GPIO.cleanup()       
+    GPIO.output(ENA,GPIO.LOW)
+    GPIO.output(IN2,GPIO.LOW)
+    #GPIO.cleanup()
     return render_template('index.html')
 
 @app.route("/left")
 def left():
-    
-    GPIO.cleanup()       
+    startup()
+    GPIO.output(IN3,GPIO.LOW)
+    GPIO.output(IN4,GPIO.HIGH)
+    GPIO.output(ENB,GPIO.HIGH)
+    #GPIO.cleanup()
     return render_template('index.html')
 
 @app.route("/right")
 def right():
-    
-    GPIO.cleanup()   
+    startup()
+    GPIO.output(IN3,GPIO.HIGH)
+    GPIO.output(IN4,GPIO.LOW)
+    GPIO.output(ENB,GPIO.HIGH)
+    #GPIO.cleanup()
     return render_template('index.html')
 
 if __name__ == "__main__":
